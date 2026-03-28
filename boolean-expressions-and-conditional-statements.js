916a6f5e8d883868db68e36b6689f0376a777319
GammaXLC <cjaya1ien22@gmail.com>
2026-03-28 13:46:01 -0500

    boolean expressions and conditional statements

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -50,3 +50,37 @@ Add Customization and expand the game:
   - Use nested conditionals and logical operators to create complex outcomes.
 
 */
+if (choice === "mountains") {
+    if (hasTorch && hasCompass) {
+        console.log("With your torch and compass, you successfully cross the peaks!");
+    } else if (hasTorch || hasCompass) {
+        console.log("You make it halfway, but you have no tools.");
+    } else {
+        console.log("It's too dark and confusing. You turn back.");
+    }
+}
+else if (choice === "village") {
+    if (hasMap || hasCompass) {
+        console.log("You easily navigate the winding roads to the village.");
+    } else {
+        console.log("The village is hidden. You wander until you find a path back home.");
+    }
+} 
+
+else if (choice === "cave") {
+    console.log("It is pitch black and you hear growling...");
+    if (hasTorch) {
+        console.log("You light your torch and see a beast!");
+        if (hasSword) {
+            console.log("You defeat the beast and find a hidden treasure!");
+        } else {
+            console.log("You have no weapon! You run away adn fall into a ditch game over!");
+        }
+    } else {
+        console.log("Without a torch, you stumble and fall into a pit. Game over!");
+    }
+} 
+// Default case
+else {
+    console.log("That path doesn't exist. You stay where you are."); 
+}
